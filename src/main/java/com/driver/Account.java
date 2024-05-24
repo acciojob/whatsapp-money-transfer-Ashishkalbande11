ca1e870b3ee5d1d0304056f4142c9b45424c986f
Ashishkalbande11 <ashishkalbande60>
2024-05-24 17:07:28 +0530

    money

--- src/main/java/com/driver/Account.java
+++ src/main/java/com/driver/Account.java
@@ -10,11 +10,23 @@ public class Account {
 
     public void sendMoney(Account receiver, int amount) {
     	// your code goes here
+        if(this.balance >= amount){
+            System.out.println(this.accountName+" sending amount " +amount);
+            this.balance -= amount;
+            receiveMoney(amount);
+            System.out.println(this.accountName+" has send amount "+amount);
+        }else{
+            System.out.println(this.accountName+" has insufficient balance.");
+        }
+
     }
 
     public void receiveMoney(int amount) {
         synchronized (this) {
         	// your code goes here
+            System.out.println(this.accountName+ " has received amount "+amount+". New balance is "+this.balance);
+            this.balance += amount;
+
         }
     }
 }
